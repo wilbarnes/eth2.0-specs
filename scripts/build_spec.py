45dbfc6da3766cbfacdc671fc22bb2886aa088c3
protolambda <proto@protolambda.com>
2019-06-19 01:03:04 +0200

    fix linting + mypy

--- scripts/build_spec.py
+++ scripts/build_spec.py
@@ -20,7 +20,7 @@ from eth2spec.utils.ssz.ssz_impl import (
     signing_root,
 )
 from eth2spec.utils.ssz.ssz_typing import (
-    Bit, Container, List, Vector, Bytes, BytesN, uint64,
+    Bit, Container, List, Vector, Bytes, uint64,
     Bytes4, Bytes32, Bytes48, Bytes96,
 )
 from eth2spec.utils.bls import (
@@ -45,7 +45,7 @@ from eth2spec.utils.ssz.ssz_impl import (
     is_empty,
 )
 from eth2spec.utils.ssz.ssz_typing import (
-    Bit, Container, List, Vector, Bytes, BytesN, uint64,
+    Bit, Container, List, Vector, Bytes, uint64,
     Bytes4, Bytes32, Bytes48, Bytes96,
 )
 from eth2spec.utils.bls import (
